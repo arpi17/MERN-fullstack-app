aaac03ea8e75fd0b8d1c7b750866d5a40fa6ca8a
Arpad Illyes <arpadillyes93@gmail.com>
2019-02-12 03:37:46 +0100

    Change email validation order

--- validation/register.js
+++ validation/register.js
@@ -3,33 +3,33 @@ const isEmpty = require('./isEmpty');
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
-  let { name, email, password, password2 } = data
-  
+  let { name, email, password, password2 } = data;
+
   // validator.isEmpty() only accepts string arguments
-  name      = !isEmpty(name)      ? name      : '';
-  email     = !isEmpty(email)     ? email     : '';
-  password  = !isEmpty(password)  ? password  : '';
+  name = !isEmpty(name) ? name : '';
+  email = !isEmpty(email) ? email : '';
+  password = !isEmpty(password) ? password : '';
   password2 = !isEmpty(password2) ? password2 : '';
 
   // Name validation
-  if (!validator.isLength(name, {min: 2, max: 30})) {
+  if (!validator.isLength(name, { min: 2, max: 30 })) {
     errors.name = 'Name must be between 2 and 30 characters';
   }
   if (validator.isEmpty(name)) {
     errors.name = 'Name field is required';
   }
   // Email validation
-  if (validator.isEmpty(email)) {
-    errors.email = 'Email field is required';
-  }
   if (!validator.isEmail(email)) {
     errors.email = 'Invalid email';
+  }
+  if (validator.isEmpty(email)) {
+    errors.email = 'Email field is required';
   }
   // Password validation
   if (validator.isEmpty(password)) {
     errors.password = 'Password field is required';
   }
-  if (!validator.isLength(password, {min: 6, max: 30})) {
+  if (!validator.isLength(password, { min: 6, max: 30 })) {
     errors.password = 'Password must be at least 6 characters';
   }
   // Password2 validation
@@ -43,5 +43,5 @@ module.exports = function validateRegisterInput(data) {
   return {
     errors,
     isValid: isEmpty(errors)
-  }
-}
+  };
+};
